023616ab8e7676d882ecb9212ca454a1ba0aee68
sjas21 <sehgaljas21@gmail.com>
2022-05-30 09:55:53 -0400

    first version of the code

---
+++ src/studentdetails/Student.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdetails;
+
+/**
+ * Model class - Raw data definition for student
+ * @author ranjeet
+ */
+public class Student {
+    private String name;//Encapsulation
+    private int age;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/studentdetails/StudentDetails.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdetails;
+
+/**
+ *
+ * @author ranjeet
+ */
+public class StudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create objects for array and print them
+        Student s1 = new Student();
+        s1.setName("Peter");
+        s1.setAge(19);
+        Student s2 = new Student();
+        s2.setName("Patrick");
+        s2.setAge(18);
+        Student s3 = new Student();
+        s3.setName("Paul");
+        s3.setAge(20);
+        Student s4 = new Student();
+        s4.setName("Paul");
+        s4.setAge(20);
+        Student s5 = new Student();
+        s5.setName("Paul");
+        s5.setAge(20);
+        
+        Student[] list= new Student[5];
+        list[0]=s1;
+        list[1]=s2;
+        list[2]=s3;
+        list[3]=s4;
+        list[4]=s5;
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(list[i].getName()+" "+list[i].getAge());
+        }
+    }
+    
+}
